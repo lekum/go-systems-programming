6034bd7a1ab620f0aa6d0016f2222b49bc0b508f
Alejandro Guirao <lekumberri@gmail.com>
2018-02-22 07:47:16 +0100

    Add another package

--- ch04/aSimplePackage/aSimplePackage.go
+++ ch04/aSimplePackage/aSimplePackage.go
@@ -3,6 +3,11 @@ package aSimplePackage
 import "fmt"
 
 const Pi = "3.14159"
+const version = "1.1"
+
+func init() {
+	fmt.Println("The init function of aSimplePackage")
+}
 
 func Add(x, y int) int {
 	return x + y
@@ -10,4 +15,8 @@ func Add(x, y int) int {
 
 func Println(x int) {
 	fmt.Println(x)
+}
+
+func Version() {
+	fmt.Println("The version of the package is", version)
 }
---
+++ ch04/usePackages/packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lekum/go-systems-programming/ch04/aSimplePackage"
+)
+
+func main() {
+	temp := aSimplePackage.Add(5, 10)
+	fmt.Println(temp)
+	fmt.Println(aSimplePackage.Pi)
+	aSimplePackage.Version()
+}

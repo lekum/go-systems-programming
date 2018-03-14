30dfb8953148f63b544731941a41f50cfda92e89
Alejandro Guirao <lekumberri@gmail.com>
2018-03-14 09:15:15 +0100

    Add flags to find

--- ch05/find/find.go
+++ ch05/find/find.go
@@ -7,21 +7,69 @@ import (
 	"path/filepath"
 )
 
+var minusS = flag.Bool("s", false, "Sockets")
+var minusP = flag.Bool("p", false, "Pipes")
+var minusSL = flag.Bool("sl", false, "Symbolic links")
+var minusD = flag.Bool("d", false, "Directories")
+var minusF = flag.Bool("f", false, "Files")
+
+var printAll bool
+
 func walkFunction(path string, info os.FileInfo, err error) error {
+
 	fileInfo, err := os.Stat(path)
 	if err != nil {
 		return err
 	}
-	mode := fileInfo.Mode()
-	if mode.IsDir() || mode.IsRegular() {
+	if printAll {
 		fmt.Println(path)
+		return nil
+	}
+	mode := fileInfo.Mode()
+	if mode.IsRegular() && *minusF {
+		fmt.Println(path)
+		return nil
+	}
+	if mode.IsDir() && *minusD {
+		fmt.Println(path)
+		return nil
+	}
+	fileInfo, _ = os.Lstat(path)
+	if fileInfo.Mode()&os.ModeSymlink != 0 {
+		if *minusSL {
+			fmt.Println(path)
+			return nil
+		}
+	}
+	if fileInfo.Mode()&os.ModeNamedPipe != 0 {
+		if *minusP {
+			fmt.Println(path)
+			return nil
+		}
+	}
+	if fileInfo.Mode()&os.ModeSocket != 0 {
+		if *minusS {
+			fmt.Println(path)
+			return nil
+		}
 	}
 	return nil
 }
 
 func main() {
+
 	flag.Parse()
 	args := flag.Args()
+
+	printAll = false
+
+	if *minusS && *minusP && *minusSL && *minusD && *minusF {
+		printAll = true
+	}
+
+	if !(*minusS || *minusP || *minusSL || *minusD || *minusF) {
+		printAll = true
+	}
 
 	if len(args) == 0 {
 		fmt.Println("Not enough arguments!")

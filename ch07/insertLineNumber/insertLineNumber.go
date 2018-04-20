3bba3ae48bc92d5885c83e3c8917c9cf6a676f92
Alejandro Guirao <lekumberri@gmail.com>
2018-04-20 08:09:15 +0200

    Add example of inserting line numbers

---
+++ ch07/insertLineNumber/insertLineNumber.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	minusINIT := flag.Int("init", 1, "Initial Value")
+	flag.Parse()
+	flags := flag.Args()
+	if len(flags) == 0 {
+		fmt.Printf("usage: insertLineNumber <files>\n")
+		os.Exit(1)
+	}
+	lineNumber := *minusINIT
+	for _, filename := range flags {
+		fmt.Println("Processing:", filename)
+		input, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+
+		lines := strings.Split(string(input), "\n")
+		for i, line := range lines {
+			lines[i] = fmt.Sprintf("%d: %s", lineNumber, line)
+			lineNumber += 1
+		}
+
+		lines[len(lines)-1] = ""
+		output := strings.Join(lines, "\n")
+		err = ioutil.WriteFile(filename, []byte(output), 0644)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+	}
+	fmt.Println("Processed:", lineNumber-*minusINIT-1, "lines")
+}

b0422ae1c17b2c865a3ea7aad5305b136fb3c43a
Alejandro Guirao <lekumberri@gmail.com>
2018-05-18 08:57:22 +0200

    Add example of log rotation

---
+++ ch07/rotateLog/rotateLog.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var TOTALWRITES int = 0
+
+const ENTRIESPERLOGFILE int = 100
+const WHENTOSTOP int = 230
+
+var openLogFile os.File
+
+func rotateLogFile(filename string) error {
+	openLogFile.Close()
+	os.Rename(filename, filename+"."+strconv.Itoa(TOTALWRITES))
+	err := setUpLogFile(filename)
+	return err
+}
+
+func setUpLogFile(filename string) error {
+	openLogFile, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+	log.SetOutput(openLogFile)
+	return nil
+}
+
+func main() {
+	numberOfLogEntries := 0
+	filename := "/tmp/myLog.log"
+	err := setUpLogFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	for {
+		log.Println(numberOfLogEntries, "This is a test log entry")
+		numberOfLogEntries++
+		TOTALWRITES++
+		if numberOfLogEntries > ENTRIESPERLOGFILE {
+			rotateLogFile(filename)
+			numberOfLogEntries = 0
+		}
+
+		if TOTALWRITES > WHENTOSTOP {
+			rotateLogFile(filename)
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Wrote", TOTALWRITES, "log entries!")
+
+}

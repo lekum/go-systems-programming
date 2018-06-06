ba9510b71bdd506e071f0cc86087ba7412825388
Alejandro Guirao <lekumberri@gmail.com>
2018-06-06 08:57:47 +0200

    Add example of interactive shell

---
+++ ch08/UnixShell/UnixShell.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const VERSION string = "0.2"
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("> ")
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Split(line, " ")
+		command := words[0]
+
+		switch command {
+		case "exit":
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		case "version":
+			fmt.Println(VERSION)
+		default:
+			fmt.Println(line)
+		}
+		fmt.Print("> ")
+	}
+}

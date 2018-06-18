a7a6d82685ce66b5f7165b21ade5247f6c10adc4
Alejandro Guirao <lekumberri@gmail.com>
2018-06-18 09:45:52 +0200

    Add a pipeline with channels for WC

--- ch09/dWC/dWC.go
+++ ch09/dWC/dWC.go
@@ -7,44 +7,84 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"sync"
 )
 
-func count(filename string) {
-	var err error
-	var numberOfLines int = 0
-	var numberOfCharacters int = 0
-	var numberOfWords int = 0
+type File struct {
+	Filename   string
+	Lines      int
+	Words      int
+	Characters int
+	Error      error
+}
 
-	f, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
+func beginProcess(files []string, out chan<- File) {
+	for _, filename := range files {
+		var fileToProcess File
+		fileToProcess.Filename = filename
+		fileToProcess.Lines = 0
+		fileToProcess.Words = 0
+		fileToProcess.Characters = 0
+		out <- fileToProcess
 	}
-	defer f.Close()
+	close(out)
+}
 
-	r := bufio.NewReader(f)
+func count(in <-chan File, out chan<- File) {
+	for y := range in {
+		filename := y.Filename
 
-	for {
-		line, err := r.ReadString('\n')
-
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			fmt.Printf("Error reading file: %s\n", err)
-			break
+		f, err := os.Open(filename)
+		if err != nil {
+			y.Error = err
+			out <- y
+			continue
 		}
-		numberOfLines++
-		r := regexp.MustCompile("[^\\s]+")
-		for range r.FindAllString(line, -1) {
-			numberOfWords++
+		defer f.Close()
+
+		r := bufio.NewReader(f)
+
+		for {
+			line, err := r.ReadString('\n')
+
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				fmt.Printf("Error reading file: %s\n", err)
+				y.Error = err
+				out <- y
+				continue
+			}
+			y.Lines += 1
+			r := regexp.MustCompile("[^\\s]+")
+			for range r.FindAllString(line, -1) {
+				y.Words += 1
+			}
+			y.Characters += len(line)
 		}
-		numberOfCharacters += len(line)
+		out <- y
 	}
-	fmt.Printf("\t%d\t", numberOfLines)
-	fmt.Printf("\t%d\t", numberOfWords)
-	fmt.Printf("\t%d\t", numberOfCharacters)
-	fmt.Printf("%s\n", filename)
+	close(out)
+}
+
+func calculate(in <-chan File) {
+	var totalWords int = 0
+	var totalLines int = 0
+	var totalChars int = 0
+	for x := range in {
+		totalWords += x.Words
+		totalLines += x.Lines
+		totalChars += x.Characters
+		if x.Error == nil {
+			fmt.Printf("\t%d\t", x.Lines)
+			fmt.Printf("%d\t", x.Words)
+			fmt.Printf("%d\t", x.Characters)
+			fmt.Printf("%s\n", x.Filename)
+		}
+
+	}
+	fmt.Printf("\t%d\t", totalLines)
+	fmt.Printf("%d\t", totalWords)
+	fmt.Printf("%d\ttotal\n", totalChars)
 }
 
 func main() {
@@ -54,13 +94,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	var wg sync.WaitGroup
-	for _, filename := range os.Args[1:] {
-		wg.Add(1)
-		go func(filename string) {
-			defer wg.Done()
-			count(filename)
-		}(filename)
-	}
-	wg.Wait()
+	files := make(chan File)
+	values := make(chan File)
+
+	go beginProcess(os.Args[1:], files)
+	go count(files, values)
+	calculate(values)
 }
